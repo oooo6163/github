b081e72f212d58851fa47a8a450fdcb83a6237f0
oooo6163 <jhon202085@gmail.com>
2024-11-15 11:51:29 +0800

    20241115

--- AgencyProject00/js/bs5vue.js
+++ AgencyProject00/js/bs5vue.js
@@ -26,3 +26,16 @@ createApp({
         }
     }
 }).mount("#portfolio")
+
+createApp({
+    data(){
+        return{
+            About:[
+                {img:"img/about/1.jpg",timelineheading:"2009-2011",heading:"Our Humble Beginnings",text:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt ut voluptatum eius sapiente, totam reiciendis temporibus qui quibusdam, recusandae sit vero unde, sed, incidunt et ea quo dolore laudantium consectetur!"},
+                {img:"img/about/2.jpg",timelineheading:"2011",heading:"An Agency is Born",text:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt ut voluptatum eius sapiente, totam reiciendis temporibus qui quibusdam, recusandae sit vero unde, sed, incidunt et ea quo dolore laudantium consectetur!"},
+                {img:"img/about/3.jpg",timelineheading:"2012",heading:"Transition to Full Service",text:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt ut voluptatum eius sapiente, totam reiciendis temporibus qui quibusdam, recusandae sit vero unde, sed, incidunt et ea quo dolore laudantium consectetur!"},
+                {img:"img/about/4.jpg",timelineheading:"2014",heading:"Phase Two Expansion",text:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt ut voluptatum eius sapiente, totam reiciendis temporibus qui quibusdam, recusandae sit vero unde, sed, incidunt et ea quo dolore laudantium consectetur!"}
+            ]
+        }
+    }
+}).mount("#about")
