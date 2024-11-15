93bfe531e5eb1bb38553c4281324f1ab8013156e
oooo6163 <jhon202085@gmail.com>
2024-11-15 12:04:52 +0800

    20241115

--- server.js
+++ server.js
@@ -6,6 +6,12 @@ var server = express();
 //web root
 server.use(express.static(__dirname+"/AgencyProject00"));
 
+var DB = require("nedb-promises"); 
+
+var Profolio = DB.create(__dirname+"/profolio.db");  
+
+Profolio.insert({modal: "#portfolioModal1", imgSrc:"roundicons.png", heading:"Round Icons", text:"Graphic Design"})
+
 server.get("/profolio", (req,res)=>{       
     //DB       
      res.send("Profolio");  })
