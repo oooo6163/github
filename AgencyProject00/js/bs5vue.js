5bd417a5ccc6778bde1df71edb2104417ebe03d2
oooo6163 <jhon202085@gmail.com>
2024-11-22 11:57:20 +0800

    20241122

--- AgencyProject00/js/bs5vue.js
+++ AgencyProject00/js/bs5vue.js
@@ -1,31 +1,36 @@
 const {createApp,ref}  = Vue;
 
-createApp({
+var myService = createApp({
     data() {
         return{
             Services:[
                 {icon: "fa-shopping-cart", heading:"E-Commerce", text:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit."},
-                {icon: "fa-laptop", heading:"Responsive Design", text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit."},
-                {icon: "fa-lock", heading:"Web Security", text:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit."}
+                {icon: "fa-laptop", heading:"Responsive Design", text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit."}
             ]
         }
     }
 }).mount("#services");
 
-createApp({
+
+myService.Services.push({icon: "fa-lock", heading:"Web Security", text:"asdf adf adf"})
+
+var vueProfolio = createApp({
     data() {
         return{
-            Portfolio:[
-                {link:"#portfolioModal1",img:"img/portfolio/roundicons.png",heading:"Round Icons",text:"Graphic Design"},
-                {link:"#portfolioModal2",img:"img/portfolio/startup-framework.png",heading:"Startup Framework",text:"Website Design"},
-                {link:"#portfolioModal3",img:"img/portfolio/treehouse.png",heading:"Treehouse",text:"Website Design"},
-                {link:"#portfolioModal4",img:"img/portfolio/golden.png",heading:"Golden",text:"Website Design"},
-                {link:"#portfolioModal5",img:"img/portfolio/escape.png",heading:"Escape",text:"Website Design"},
-                {link:"#portfolioModal6",img:"img/portfolio/dreams.png",heading:"Dreams",text:"Website Design"}
-            ]
+            Portfolio:[]
         }
     }
 }).mount("#portfolio")
+
+$.ajax({
+    url:"/profolio",
+    method: "get",
+    dataType: "json",
+    success: results=>{
+        vueProfolio.Portfolio = results;
+    }
+
+})
 
 createApp({
     data(){
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 //install: node js
 //install web server package: express >npm install express
+//install web server package: express >npm install nedb-promises
 var express = require("express");
 var server = express();
 
@@ -8,14 +9,44 @@ server.use(express.static(__dirname+"/AgencyProject00"));
 
 var DB = require("nedb-promises"); 
 
-var Profolio = DB.create(__dirname+"/profolio.db");  
+var ProfolioDB = DB.create(__dirname+"/profolio.db");
 
-Profolio.insert({modal: "#portfolioModal1", imgSrc:"roundicons.png", heading:"Round Icons", text:"Graphic Design"})
+ 
+ //ProfolioDB.insert([
+ //    { href: "#portfolioModal1", imgSrc: "img/portfolio/roundicons.png", title: "Round Icons", text: "Graphic Design" },
+ //    { href: "#portfolioModal2", imgSrc: "img/portfolio/startup-framework.png", title: "Startup Framework", text: "Website Design" },
+ //    { href: "#portfolioModal3", imgSrc: "img/portfolio/treehouse.png", title: "Treehouse", text: "Website Design" },
+ //    { href: "#portfolioModal1", imgSrc: "img/portfolio/roundicons.png", title: "Round Icons", text: "Graphic Design" },
+ //    { href: "#portfolioModal2", imgSrc: "img/portfolio/startup-framework.png", title: "Startup Framework", text: "Website Design" },
+ //    { href: "#portfolioModal3", imgSrc: "img/portfolio/treehouse.png", title: "Treehouse", text: "Website Design" }
+ //])
+server.get("/services", (req, res)=>{
+    //DB find
+    var Services=[
+        {icon: "fa-shopping-cart", heading:"E-Commerce", text:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit."},
+        {icon: "fa-laptop", heading:"Responsive Design", text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit."}
+    ];
+    res.send(Services);
+});
 
-server.get("/profolio", (req,res)=>{       
-    //DB       
-     res.send("Profolio");  })
+server.get("/profolio", (req,res)=>{
+    //DB
+    ProfolioDB.find({}).then(results=>{
+      if(results != null){
+           res.send(results);
+      }else{
+          res.send("Error!");
+      }
+    })
+   
+
+})
+
+server.post("/contact_me",(req,res)=>{
+    ContactDB.insert(req.body);
+    res.send("OK");
+})
 
 server.listen(80, ()=>{
-    console.log("Server is running at port 80.");
+  console.log("Server is running at port 80.");
 })
