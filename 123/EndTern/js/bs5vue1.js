67e2679d0f179b165a397f8884bd5da39345a0cc
oooo6163 <jhon202085@gmail.com>
2024-12-20 00:27:27 +0800

    20241220

--- 123/EndTern/js/bs5vue1.js
+++ 123/EndTern/js/bs5vue1.js
@@ -1,15 +1,33 @@
-const {createApp,ref}  = Vue;
+const { createApp } = Vue;
 
-
-
-var vueShop = createApp({
-    data() {
-      return {
-        Tile: [
-          { img:"img/img1.jpg" ,text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt ut voluptatum eius sapiente, totam reiciendis temporibus qui quibusdam, recusandae sit vero unde, sed, incidunt et ea quo dolore laudantium consectetur!" },
-          { img:"img/img2.jpg" ,text: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo." },
-          { img:"img/img3.jpeg" ,text: "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi." }
-        ]
-      };
+createApp({
+  data() {
+    return {
+      Tile: [
+        { img: "img/img1.jpg", text: "商品 1 描述",price:"500" ,category: "分類一" },
+        { img: "img/img2.jpg", text: "商品 2 描述", price:"600",category: "分類二" },
+        { img: "img/img3.jpeg", text: "商品 3 描述", price:"500",category: "分類三" },
+        { img: "img/img1.jpg", text: "商品 4 描述", price:"300",category: "分類一" },
+        { img: "img/img2.jpg", text: "商品 5 描述",price:"800" ,category: "分類二" },
+        { img: "img/img3.jpeg", text: "商品 6 描述",price:"900" ,category: "分類四" }
+      ],
+      popularCategories: ["分類一", "分類二", "分類三", "分類四"], // 熱門分類
+      selectedCategory: "" // 當前選中的分類
+    };
+  },
+  methods: {
+    filterByCategory(category) {
+      this.selectedCategory = category; // 設定篩選的分類
+      console.log(`選中的分類: ${category}`); // 查看選中的分類是否正確
     }
-  }).mount("#shop");
+  },
+  computed: {
+    filteredTiles() {
+      const result = !this.selectedCategory
+        ? this.Tile
+        : this.Tile.filter((item) => item.category === this.selectedCategory);
+      console.log("篩選結果:", result); // 查看篩選結果是否正確
+      return result;
+    }
+  }
+}).mount("#shop");
